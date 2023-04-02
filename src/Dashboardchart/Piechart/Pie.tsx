934f959d6297888fc5ff6cd57a393238a5e9e952
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-04-02 16:58:34 +0530

    code

--- src/Dashboardchart/Piechart/Pie.tsx
+++ src/Dashboardchart/Piechart/Pie.tsx
@@ -45,13 +45,6 @@ function Pie() {
             indexLabelFontSize: 16,
             indexLabel: "{label} - {y}",
             dataPoints: Pieline.map((item: any) => ({ y: item.value,label: item.label }))
-            // dataPoints: [
-            //     { y: 18, label: "Direct" },
-            //     { y: 49, label: "Organic Search" },
-            //     { y: 9, label: "Paid Search" },
-            //     { y: 5, label: "Referral" },
-            //     { y: 19, label: "Social" }
-            // ]
         }]
     };
 
